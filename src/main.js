3b131bc76978ea201787dd79fc0a83d7f95888aa
Vikrant Payal <vikrantpayal@gmail.com>
2023-11-30 01:51:55 +0000

    .

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@
 import L from 'leaflet';
 
 // Initialize the map
-const map = L.map('map').setView([12.971598, 77.594562], 13); // Set initial view to Bangalore, India
+const map = L.map('map').setView([51,-0], 13); // Set initial view to Bangalore, India
 
 // Add a tile layer (OpenStreetMap)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+ 
